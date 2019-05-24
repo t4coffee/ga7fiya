71713cc6f2e4c9fd3b4b49184d22f04b3a96c38b
t4coffee <t4coffee@gmail.com>
2019-05-25 00:48:08 +0300

    DOCX Content Control with XML
    https://docs.microsoft.com/en-us/visualstudio/vsto/walkthrough-binding-content-controls-to-custom-xml-parts?view=vs-2019

---
+++ WordDocument1/ThisDocument.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Office.Tools.Word;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WordDocument1
+{
+    public partial class ThisDocument
+    {
+        [CachedAttribute()]
+        public string employeeXMLPartID = string.Empty;
+        private Office.CustomXMLPart employeeXMLPart;
+        private const string prefix = "xmlns:ns='http://schemas.microsoft.com/vsto/samples'";
+
+        private void ThisDocument_Startup(object sender, System.EventArgs e)
+        {
+            string xmlData = GetXmlFromResource();
+
+            if (xmlData != null)
+            {
+                AddCustomXmlPart(xmlData);
+                BindControlsToCustomXmlPart();
+            }
+
+        }
+
+        private void ThisDocument_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        private string GetXmlFromResource()
+        {
+            System.Reflection.Assembly asm =
+                System.Reflection.Assembly.GetExecutingAssembly();
+            System.IO.Stream stream1 = asm.GetManifestResourceStream(
+                "WordDocument1.employees.xml");
+
+            using (System.IO.StreamReader resourceReader =
+                    new System.IO.StreamReader(stream1))
+            {
+                if (resourceReader != null)
+                {
+                    return resourceReader.ReadToEnd();
+                }
+            }
+
+            return null;
+        }
+
+        private void AddCustomXmlPart(string xmlData)
+        {
+            if (xmlData != null)
+            {
+                employeeXMLPart = this.CustomXMLParts.SelectByID(employeeXMLPartID);
+                if (employeeXMLPart == null)
+                {
+                    employeeXMLPart = this.CustomXMLParts.Add(xmlData);
+                    employeeXMLPart.NamespaceManager.AddNamespace("ns",
+                        @"http://schemas.microsoft.com/vsto/samples");
+                    employeeXMLPartID = employeeXMLPart.Id;
+                }
+            }
+        }
+
+        private void BindControlsToCustomXmlPart()
+        {
+            string xPathName = "ns:employees/ns:employee/ns:name";
+            this.plainTextContentControl1.XMLMapping.SetMapping(xPathName,
+                prefix, employeeXMLPart);
+
+            string xPathDate = "ns:employees/ns:employee/ns:hireDate";
+            this.datePickerContentControl1.DateDisplayFormat = "MMMM d, yyyy";
+            this.datePickerContentControl1.XMLMapping.SetMapping(xPathDate,
+                prefix, employeeXMLPart);
+
+            string xPathTitle = "ns:employees/ns:employee/ns:title";
+            this.dropDownListContentControl1.XMLMapping.SetMapping(xPathTitle,
+                prefix, employeeXMLPart);
+        }
+
+        #region VSTO Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisDocument_Startup);
+            this.Shutdown += new System.EventHandler(ThisDocument_Shutdown);
+        }
+
+        #endregion
+    }
+}
