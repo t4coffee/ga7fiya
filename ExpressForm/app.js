3ddfa998002eef716dad48d7e06ccb93d75d6155
t4coffee <t4coffee@gmail.com>
2019-06-05 01:04:32 +0300

    Clean version
    Open an xml file
    Parses content into a js string
    Converts it to a JSON string
    Builds it into an xml 
    Saves it to another file

--- ExpressForm/app.js
+++ ExpressForm/app.js
@@ -13,6 +13,8 @@ var app = express();
 var fs = require('fs');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
+var DOMParser = require('xmldom').DOMParser;
+var XMLSerializer = require ('xmldom').XMLSerializer;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,62 +29,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-/* route redirect to form page - actually being done by index.js
-app.get('/', function(req, res){
-  res.render('form');
-});
-*/
 
-/* form post function that will read an xml file into a variable and display the contents to the console
-app.post('/submitform', function(req,res){
-  var xmlfile  = __dirname + "/public/xml/formdata.xml";
-
-  fs.readFile(xmlfile, "utf-8", function(err, data) {
-    if (err) {
-      throw error;
-    }
-    else {
-      parser.parseString(data, function (err, result) {
-        console.dir(result[1]);
-        console.log('Done');
-      });
-    }
-  });
-
-  return res.send(req.body);
-});
-*/
+var THEHULK = 'NOTHING HERE';
 
 app.post('/submitform', function(req,res){
-  var xmlfile  = __dirname + "/public/xml/formdata2.xml";
 
-  var builder = new xml2js.Builder();
-  var parsedxml = builder.buildObject(req.body);
-  console.log(parsedxml);
+// Define path to source xml file
+  var sourcexml  = __dirname + "/public/xml/kam.xml";
 
-// Replace contents of XML file with XMLContent from form
-  fs.writeFile(xmlfile, parsedxml, function (err) {
-    if (err) throw err;
-    console.log('Replaced!');
+// Nodejs file system method to read contents of an xml file into an object (stringify to convert the object to a JSON)
+  fs.readFile(sourcexml, "utf-8", function(err, data) {
+    parser.parseString(data, function(err, result) {
+        // Parse the XML from source to a JSON object - for easy editing
+        THEHULK = JSON.stringify(result, null, 2);
+        console.log(THEHULK);
+
+        // Define path to new XML file, build out the content as a Javascript Object
+          var destfile  = __dirname + "/public/xml/formdata2.xml";
+          var builder = new xml2js.Builder();
+          var dxml = builder.buildObject(result);
+
+          //Nodejs file system method to write content of destination xml object to destination file
+          fs.writeFile(destfile, dxml, function (err) {
+            if (err) throw err;
+            console.log('Replaced!');
+          });
+    });
   });
 
+//What to show on the page after the submit button is clicked on the form
  return res.send(req.body);
 
 });
-
-
-/* Open XML file and write out content to the console
-var xmlfile  = __dirname + "/public/xml/formdata.xml";
-
-  fs.readFile(xmlfile, "utf-8", function(err, data) {
-    parser.parseString(data, function (err, result) {
-      console.dir(JSON.stringify(result.salesform.salesteam[1].firstname));
-    });
-  });
-  return res.send(req.body);
-
-  });
-*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
